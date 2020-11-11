fcab1e7df43305d9df259588a7119455d36d758d
George <grechin58@mail.ru>
2020-11-11 12:47:27 +0300

    Initial Commit

---
+++ MyPlaces#5Tests/MyPlaces_5Tests.swift
@@ -0,0 +1,33 @@
+//
+//  MyPlaces_5Tests.swift
+//  MyPlaces#5Tests
+//
+//  Created by Георгий Гречин on 11.11.2020.
+//
+
+import XCTest
+@testable import MyPlaces_5
+
+class MyPlaces_5Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
